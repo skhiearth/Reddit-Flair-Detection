ac0863b59cf242109ae7986200a288cd2ae28ce6
Utkarsh Sharma <skhiearth@gmail.com>
2020-04-09 15:56:38 +0530

    Parse and download data

--- ExperimentLog.md
+++ ExperimentLog.md
@@ -2,3 +2,9 @@
 
 ## Stage 1: Data Collection
 
+**9 April, 2020**:
+
+1. Create a Reddit Application to get OAuth2 keys. [SUCCESS]
+2. Creating the Python script which will be used to parse data. [SUCCESS]
+    2.1. Getting Reddit and Subreddit instances. [SUCCESS]
+    2.2. Parsing and Downloading the data [SUCCESS]
---
+++ RedditData/redditData.py
@@ -0,0 +1,30 @@
+# Import required packages
+from account import id, secret, appName, username
+import praw # Python Reddit API Wrapper
+import pandas as pd 
+import datetime as dt
+
+# Reddit and subreddit instances
+reddit = praw.Reddit(client_id=id, \
+                     client_secret=secret, \
+                     user_agent=appName, \
+                     username=username)
+
+subreddit = reddit.subreddit('india')
+
+# Getting the top 100 posts in the subreddit
+top_subreddit = subreddit.top(limit=1000)
+
+# Parsing and Downloading the data
+topics_dict = {"title":[], "score":[], "id":[], "url": [], "body":[], "flair":[]}
+
+for submission in top_subreddit:
+    topics_dict["title"].append(submission.title)
+    topics_dict["score"].append(submission.score)
+    topics_dict["id"].append(submission.id)
+    topics_dict["url"].append(submission.url)
+    topics_dict["body"].append(submission.selftext)
+    topics_dict["flair"].append(submission.link_flair_text)
+
+topics_data = pd.DataFrame(topics_dict)
+topics_data.to_csv('RedditData/Data/raw.csv', index=False) 
