6dff9ca20c1fda57dc64f735540fc3d9bc9963c4
Utkarsh Sharma <skhiearth@gmail.com>
2020-04-23 16:48:21 +0530

    single url prediction succesful

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/results", methods=['POST'])
+@app.route("/automated_testing", methods=['POST'])  
 def predict():
 
     with open('Models/LSTMTokenizer.pickle', 'rb') as handle:
@@ -31,13 +31,19 @@ def predict():
     if request.method == 'POST':
         comment = request.form['comment']
 
+        def remove_prefix(text, prefix):
+            return text[text.startswith(prefix) and len(prefix):]
+
+        comment = remove_prefix(comment, 'https://www.reddit.com/r/india/comments/')
+        comment = comment.split('/', 1)[0]
+
         # Reddit and subreddit instances
         reddit = praw.Reddit(client_id=id, \
                             client_secret=secret, \
                             user_agent=appName, \
                             username=username)
 
-        submission = reddit.submission(id='g6gz0e')
+        submission = reddit.submission(id=comment)
         title = submission.title
         body = submission.selftext
 
@@ -50,7 +56,7 @@ def predict():
         my_prediction = model.predict(vect)
         my_prediction = np.argmax(my_prediction, axis=1)
         print(my_prediction)
-    return render_template("results.html", prediction=my_prediction, comment=text_to_classify)
+    return render_template("automated_testing.html", prediction=my_prediction, comment=text_to_classify)
 
 if __name__ == '__main__':
     app.run(debug=True)
